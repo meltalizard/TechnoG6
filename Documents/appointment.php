b89b1b16eb95cb25b2d8651b7eff7101311e35c0
meltalizard <school.mikotancinco@gmail.com>
2025-04-26 22:27:55 +0800

    second

---
+++ Documents/appointment.php
@@ -0,0 +1,33 @@
+<form action="register.php" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required><br>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" required><br>
+
+    <label for="pnumber">Phone Number:</label>
+    <input type="text" name="pnumber" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" required><br>
+
+    <label for="street_address">Street Address:</label>
+    <input type="text" name="street_address" required><br>
+
+    <label for="apartment_unit_suite">Apartment/Unit/Suite (Optional):</label>
+    <input type="text" name="apartment_unit_suite"><br>
+
+    <label for="barangay">Barangay:</label>
+    <input type="text" name="barangay" required><br>
+
+    <label for="city_municipality">City / Municipality:</label>
+    <input type="text" name="city_municipality" required><br>
+
+    <label for="province">Province:</label>
+    <input type="text" name="province" required><br>
+
+    <label for="postal_code">Postal Code:</label>
+    <input type="text" name="postal_code" required><br>
+
+    <input type="submit" value="Register">
+</form>
